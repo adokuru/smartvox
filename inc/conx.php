c88738230cfa30da753fafa6d50a9bf6ef4e197c
David Adokuru <davidadokuru@gmail.com>
2020-05-19 10:45:31 +0100

    elect updates

--- inc/conx.php
+++ inc/conx.php
@@ -3,21 +3,12 @@
 	session_start();
 	
 	define("host", "localhost");
-	define("user", "danoitec_david");
-	define("password", "davidadokuru123");
+	define("user", "root");
+	define("password", "");
 	define("dbase", "danoitec_smartbox");
 
 	$conx = mysqli_connect(host, user, password, dbase);
-
 	if (!$conx) {
 		echo "Database connection not found";
-		define("host1", "localhost");
-		define("user1", "root");
-		define("password1", "");
-		define("dbase1", "danoitec_smartbox");
-		$conx1 = mysqli_connect(host1, user1, password1, dbase1);
-		if (!$conx1) {
-		echo "Database connection not found";
-		}
 	}
 ?>
